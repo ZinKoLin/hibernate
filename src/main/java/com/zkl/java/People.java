a529c3c01f74e441247363740a643771844a87e1
zinkolin <zinkolin113@gmail.com>
2021-06-30 21:22:02 +0630

    first commit

---
+++ src/main/java/com/zkl/java/People.java
@@ -0,0 +1,49 @@
+package com.zkl.java;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table
+
+public class People {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+
+	
+	private long id;
+	
+	@Column(length=30)
+	private String name;
+	
+	@Column(length=3)
+	private int age;
+
+	public long getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+}
